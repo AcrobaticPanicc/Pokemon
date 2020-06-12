a2cda9e26e2c5c6cbdbaa6da1eda0ae97b39a4db
AcrobaticPanicc <tomeros@gmail.com>
2020-06-13 01:06:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-from bs4 import BeautifulSoup
-import requests
 import os
 import shutil
+import requests
 from pprint import pprint
+from bs4 import BeautifulSoup
+
+shutil.rmtree("pokemons", ignore_errors=True)
+os.mkdir("pokemons")
 
 src = requests.get('http://pokemondb.net/pokedex/national')
-soup = BeautifulSoup(src.content, 'lxml')
-generation = soup.find_all('div', attrs={'class': 'infocard-list infocard-list-pkmn-lg'})
+
+soup = BeautifulSoup(src.content, 'html.parser')
+generations = soup.find_all('div', attrs={'class': 'infocard-list infocard-list-pkmn-lg'})
 pk_name = soup.find_all('a', attrs={'class': 'ent-name'})
 
-shutil.rmtree("pokemons")
-os.mkdir("pokemons")
-s = 1
 
-for g in generation:
+s = 1
+for generation in generations:
     os.mkdir(f"pokemons/season {s}")
-    pokemons = g.find_all('div', attrs={'class': 'infocard'})
-    for p in pokemons:
-        pokemon_name = p.select("span.infocard-lg-data a.ent-name")[0].text
-        img_url = p.select("span.infocard-lg-img a span")[0]["data-src"]
+    pokemons = generation.find_all('div', attrs={'class': 'infocard'})
+
+    for pokemon in pokemons:
+        pokemon_name = pokemon.select("span.infocard-lg-data a.ent-name")[0].text
+        print(pokemon.select("span.infocard-lg-img a span"))
+        img_url = pokemon.select("span.infocard-lg-img a span")[0]["data-src"]
         img_content = requests.get(img_url).content
         with open(f'pokemons/season {s}/{pokemon_name}.jpg', 'wb') as f:
             f.write(img_content)
     s += 1
+
 
